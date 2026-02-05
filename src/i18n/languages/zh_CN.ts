06b8adb9fde4101b060880db1a0303ce10fac313
Britney <2855057900@qq.com>
2026-02-05 10:14:57 +0800

    feat: Add Amigos

--- src/i18n/languages/zh_CN.ts
+++ src/i18n/languages/zh_CN.ts
@@ -6,7 +6,8 @@ export const zh_CN: Translation = {
 	[Key.about]: "关于",
 	[Key.archive]: "归档",
 	[Key.search]: "搜索",
-
+	[Key.friends]: "友链",
+	
 	[Key.tags]: "标签",
 	[Key.categories]: "分类",
 	[Key.recentPosts]: "最新文章",
